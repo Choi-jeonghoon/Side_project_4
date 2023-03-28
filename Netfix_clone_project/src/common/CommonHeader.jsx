ee8842faafe41fe2ea8b33ea7bf358a99794df3e
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-03-28 13:15:59 +0900

    Add:HeaderUI

--- Netfix_clone_project/src/common/CommonHeader.jsx
+++ Netfix_clone_project/src/common/CommonHeader.jsx
@@ -1,7 +1,53 @@
-import React, { memo } from 'react';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 
-const CommonHeader = memo(() => {
-  return <div>여기는 헤더 메뉴</div>;
-});
+const navItems = [
+  "Home",
+  "Series",
+  "Movie",
+  "NEW!!요즘 대세 콘텐츠",
+  "Wish List",
+];
+
+const CommonHeader = () => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar component="nav" sx={{ background: "black" }}>
+        <Toolbar>
+          <Box>
+            <img
+              src="/logo.png"
+              alt="logo"
+              style={{
+                width: "120px",
+              }}
+            />
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" }, pl: "4%" }}>
+            <Box sx={{ display: "flex" }}>
+              {navItems.map(item => (
+                <Box
+                  key={item}
+                  sx={{
+                    color: "#fff",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    px: "10px",
+                    typography: "body1",
+                  }}
+                >
+                  {item}
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </Box>
+  );
+};
 
 export default CommonHeader;
