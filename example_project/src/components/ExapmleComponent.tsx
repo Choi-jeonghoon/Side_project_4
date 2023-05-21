bc790d623d84f4d7b87546c2234bc3fe7fd1e02f
최정훈 <jeongssi94@gmail.com>
2023-05-22 06:19:30 +0900

    Add: tab기능

--- example_project/src/components/ExapmleComponent.tsx
+++ example_project/src/components/ExapmleComponent.tsx
@@ -1,9 +1,29 @@
 import { SetStateAction, useEffect, useRef, useState } from 'react';
+import { Box, Button } from '@mui/material';
+
+type ButtonInfo = {
+  text: string;
+  height?: string;
+};
+
+const buttons: ButtonInfo[] = [
+  { text: 'AAAA' },
+  { text: 'BBBB' },
+  { text: 'DDDD' },
+  { text: 'CCCC' },
+  { text: 'EEEE' },
+  { text: 'FFFF' },
+  { text: 'GGGG' },
+  { text: 'HHHH' },
+  { text: 'HHHH' },
+  { text: 'HHHH' },
+  { text: 'HHHH' },
+];
 
 const MobileTab = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const tabListRef = useRef(null);
-  const tabContainerRef = useRef(null);
+  const tabListRef = useRef<HTMLDivElement>(null);
+  const tabContainerRef = useRef<HTMLDivElement>(null);
 
   const handleTabClick = (index: SetStateAction<number>) => {
     setSelectedIndex(index);
@@ -11,62 +31,45 @@ const MobileTab = () => {
 
   useEffect(() => {
     const container = tabContainerRef.current;
-    const selectedTab = tabListRef.current.children[selectedIndex];
-    const containerWidth = container.offsetWidth;
-    const tabWidth = selectedTab.offsetWidth;
-    const tabOffsetLeft = selectedTab.offsetLeft;
-    const tabCenterOffset = tabOffsetLeft - containerWidth / 4 + tabWidth / 4;
-    const scrollLeft = Math.max(0, tabCenterOffset - containerWidth / 5);
-    container.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+    const selectedTab = tabListRef.current?.children[selectedIndex] as HTMLButtonElement;
+    if (selectedTab && container) {
+      const containerWidth = container.offsetWidth;
+      const tabWidth = selectedTab.offsetWidth;
+      const tabOffsetLeft = selectedTab.offsetLeft;
+      const tabCenterOffset = tabOffsetLeft - containerWidth / 4 + tabWidth / 4;
+      const scrollLeft = Math.max(0, tabCenterOffset - containerWidth / 5);
+      container.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+    }
   }, [selectedIndex]);
 
   return (
-    <div style={{ width: '390px', margin: '0 auto' }}>
-      <div ref={tabContainerRef} style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-        <div ref={tabListRef} style={{ display: 'flex', justifyContent: 'center' }}>
-          <button
-            style={{ height: '150px' }}
-            className={selectedIndex === 0 ? 'active' : ''}
-            onClick={() => handleTabClick(0)}
-          >
-            AAAAAAAA
-          </button>
-          <button className={selectedIndex === 1 ? 'active' : ''} onClick={() => handleTabClick(1)}>
-            BBBBBBBB
-          </button>
-          <button className={selectedIndex === 2 ? 'active' : ''} onClick={() => handleTabClick(2)}>
-            DDDD
-          </button>
-          <button className={selectedIndex === 3 ? 'active' : ''} onClick={() => handleTabClick(3)}>
-            CCCCCCCC
-          </button>
-          <button className={selectedIndex === 4 ? 'active' : ''} onClick={() => handleTabClick(4)}>
-            EEEEEEEE
-          </button>
-          <button className={selectedIndex === 5 ? 'active' : ''} onClick={() => handleTabClick(5)}>
-            FFFFFFFFF
-          </button>
-          <button className={selectedIndex === 6 ? 'active' : ''} onClick={() => handleTabClick(6)}>
-            GGGGGGGGG
-          </button>
-          <button className={selectedIndex === 7 ? 'active' : ''} onClick={() => handleTabClick(7)}>
-            HHHHHHHHH
-          </button>
-          <button className={selectedIndex === 8 ? 'active' : ''} onClick={() => handleTabClick(8)}>
-            HHHHHHHHH
-          </button>
-          <button className={selectedIndex === 9 ? 'active' : ''} onClick={() => handleTabClick(9)}>
-            HHHHHHHHH
-          </button>
-          <button
-            className={selectedIndex === 10 ? 'active' : ''}
-            onClick={() => handleTabClick(10)}
-          >
-            HHHHHHHHH
-          </button>
-        </div>
-      </div>
-    </div>
+    <Box sx={{ width: '390px', margin: '0 auto', ml: 1 }}>
+      <Box
+        ref={tabContainerRef}
+        sx={{ overflowX: 'auto', whiteSpace: 'nowrap', padding: '10px', flexWrap: 'nowrap' }}
+      >
+        <Box ref={tabListRef} sx={{ justifyContent: 'center', alignItems: 'center' }}>
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              variant='contained'
+              size='large'
+              sx={{
+                height: button.height || '40px',
+                minWidth: '80px', // 버튼의 최소 가로 크기를 지정
+                '&.active': {
+                  backgroundColor: 'gray',
+                },
+              }}
+              className={selectedIndex === index ? 'active' : ''}
+              onClick={() => handleTabClick(index)}
+            >
+              {button.text}
+            </Button>
+          ))}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
